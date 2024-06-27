6b781fb6c8f6792ef4731699a363c0fa768e4371
Abhiram S <118326183+AGSCRIPTS@users.noreply.github.com>
2024-06-27 08:28:40 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,17 +1,1 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterServerEvent('checkEMSAvailability')
-AddEventHandler('checkEMSAvailability', function()
-    local src = source
-    local emsAvailable = false
-    
-    for _, player in pairs(QBCore.Functions.GetPlayers()) do
-        local Player = QBCore.Functions.GetPlayer(player)
-        if Player.PlayerData.job.name == 'ambulance' then
-            emsAvailable = true
-            break
-        end
-    end
-    
-    TriggerClientEvent('notifyEMSAvailability', src, emsAvailable)
-end)
+-- not till now
